d83f96714b2f83e266ca4d24bf17591db5a279a3
dzambato <kenhim@mail.ru>
2018-11-08 08:14:03 +0300

    Initial commit

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,38 @@
+module.exports = function () {
+    $.gulp.task('styles:build', () => {
+        return $.gulp.src([
+            './dev/static/sass/index.sass'
+        ])
+            .pipe($.gp.sass({
+            }))
+            .pipe($.gp.autoprefixer({
+                browsers: ['last 3 version']
+            }))
+            .pipe($.gp.csscomb())
+            .pipe($.gp.csso())
+            .pipe($.gulp.dest('./build/static/css/'))
+    });
+
+    $.gulp.task('styles:dev', () => {
+        return $.gulp.src([
+            './dev/static/sass/index.sass'
+        ])
+            .pipe($.gp.sourcemaps.init())
+            .pipe($.gp.sass({
+            }))
+            .on('error', $.gp.notify.onError(function (error) {
+                return {
+                    title: 'sass',
+                    message: error.message
+                };
+            }))
+            .pipe($.gp.sourcemaps.write())
+            .pipe($.gp.autoprefixer({
+                browsers: ['last 3 version']
+            }))
+            .pipe($.gulp.dest('./build/static/css/'))
+            .pipe($.browserSync.reload({
+                stream: true
+            }));
+    });
+};
